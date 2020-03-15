389bd379d5588dd93a42613d0e7d4c4588421687
pt0918 <58218584+pt0918@users.noreply.github.com>
2020-03-15 22:16:39 +0800

    Add files via upload

---
+++ Search.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<string.h>
+int ergodic(int t[], int len, int x) // 遍历检索
+{
+	for (int i = 0; i < len; i++)
+	{
+		if (t[i] == x)
+			return i+1;
+	}
+	return 0;
+}
+int binary(int t[], int len, int x)// 二分检索
+{
+	int l = 0;
+	int i = 0;
+	while (l <= len) {
+		i = l + (len - l) / 2;
+		if (x < t[i])
+			len = i - 1;
+		else if (x > t[i])
+			l = i + 1;
+		else 
+			return i+1;
+	}
+	return 0;
+}
+int main()
+{
+	int t[10] = { 1,3,4,6,8,9,10,11,14,15 };//排好序的数组T
+	int x;
+	scanf("%d", &x);
+	printf("遍历检索:%d\n", ergodic(t, 10, x));
+	printf("二分检索:%d\n", binary(t, 10, x));
+	return 0;
+}
