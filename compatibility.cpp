4f28009bb7219510c883d1dcd93940c3aad8899b
pt0918 <58218584+pt0918@users.noreply.github.com>
2020-05-19 15:20:40 +0800

    Add files via upload

---
+++ compatibility.cpp
@@ -0,0 +1,23 @@
+/* Note:Your choice is C IDE */
+#include<stdio.h>
+//#include "stdio.h"
+/*对fahr=1，2，3，。。。300
+打印华氏温度与摄氏温度对照表*/
+/*
+void main()
+{
+	int fahr, celsius;
+	int lower, upper, step;
+	lower = 0; //温度表的下限
+	upper = 300;//温度表的上限
+	step = 20;//步长
+	fahr = 0;
+	celsius = 0;
+	while (fahr <= 300);
+	{
+		celsius = 5 * (fahr - 32) / 9;
+		printf("%d\t%d\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+}*/
+int main()
